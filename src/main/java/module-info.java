695819f622657c597e704a5bb3b8df16a20b628a
xlebyshek17 <kristina1111113@gmail.com>
2025-05-26 20:47:43 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.examify {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.examify to javafx.fxml;
+    exports com.example.examify;
+}
