c4ca2ac779f279d388ea6e8db7bda467d52eda5c
xlebyshek17 <kristina1111113@gmail.com>
2025-05-26 22:48:02 +0200

    added WelcomePage files

---
+++ src/main/java/com/example/examify/ui/controllers/WelcomeController.java
@@ -0,0 +1,33 @@
+package com.example.examify.ui.controllers;
+
+import com.example.examify.MainApp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class WelcomeController {
+    @FXML
+    protected void onSignInButtonClick(ActionEvent event) throws IOException {
+        Parent newRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/examify/fxml/login-view.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(newRoot));
+        stage.setTitle("Examify — Logowanie");
+        stage.show();
+    }
+
+    @FXML
+    protected void onSignUpButtonClick(ActionEvent event) throws IOException {
+        Parent newRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/examify/fxml/signup-view.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(newRoot));
+        stage.setTitle("Examify — Registration");
+        stage.show();
+    }
+}
