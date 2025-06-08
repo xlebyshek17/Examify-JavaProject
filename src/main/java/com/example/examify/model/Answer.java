ae5accb81dadc2d93cdd7dd01413eb1d6e42d3bc
xlebyshek17 <kristina1111113@gmail.com>
2025-06-08 03:53:43 +0200

    added answer model

---
+++ src/main/java/com/example/examify/model/Answer.java
@@ -0,0 +1,57 @@
+package com.example.examify.model;
+
+public class Answer {
+    private int id;
+    private int examId;
+    private int questionId;
+    private String answer;
+    private boolean isCorrect;
+
+    Answer(int id, int examId, int questionId, String answer, boolean isCorrect) {
+        this.id = id;
+        this.examId = examId;
+        this.questionId = questionId;
+        this.answer = answer;
+        this.isCorrect = isCorrect;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getExamId() {
+        return examId;
+    }
+
+    public void setExamId(int examId) {
+        this.examId = examId;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+}
