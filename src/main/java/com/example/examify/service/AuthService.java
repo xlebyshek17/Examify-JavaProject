099eeb97cd87e06aeb7672ca12e760fb5879928a
xlebyshek17 <kristina1111113@gmail.com>
2025-06-10 20:05:21 +0200

    added password hashing

--- src/main/java/com/example/examify/service/AuthService.java
+++ src/main/java/com/example/examify/service/AuthService.java
@@ -2,6 +2,8 @@ package com.example.examify.service;
 
 import com.example.examify.dao.UserDAO;
 import com.example.examify.model.User;
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.util.Optional;
 
 public class AuthService {
@@ -16,18 +18,18 @@ public class AuthService {
             return RegistrationResult.EMAIL_EXISTS;
         }
 
-        // brakuje funkcji haszującej
-        // String hash =
+        String salt = BCrypt.gensalt(12);
+        String hashPassword = BCrypt.hashpw(plainPassword, salt);
 
-        User newUser = new User(username, email, plainPassword);
+        User newUser = new User(username, email, hashPassword);
         return userDAO.save(newUser) ? RegistrationResult.OK : RegistrationResult.ERROR;
     }
 
-    public Optional<User> login(String login, String plainPassword) {
+    public Optional<User> login(String login, String inputPassword) {
         Optional<User> optUser = userDAO.findByLogin(login);
         if (optUser.isPresent()) {
             User user = optUser.get();
-            if (plainPassword.equals(user.getPasswordHash())) {
+            if (BCrypt.checkpw(inputPassword, user.getPasswordHash())) {
                 return Optional.of(user);
             }
         }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.example.examify {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires org.xerial.sqlitejdbc;
+    requires jbcrypt;
 
     // Otwieramy pakiety z kontrolerami, żeby FXMLLoader mógł je wstrzyknąć
     opens com.example.examify.ui.controllers to javafx.fxml;
